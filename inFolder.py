742db1df83bc3d2f82ef921f95c23672e05a9915
CreateModMaster <alex.inns@caterhamschool.co.uk>
2026-07-25 17:13:51 +0100

    Changed inFolder.py to match the newest algorithm

--- inFolder.py
+++ inFolder.py
@@ -1,19 +1,21 @@
 import json
 import os
 
-if os.path.exists("pythonTracker.json"):
-    with open("pythonTracker.json", "r") as f:
-        currentModsDict = json.load(f)
-        currentModsList = currentModsDict["mods"]
-        print(currentModsList)
-else:
-    with open("pythonTracker.json", "w") as f:
-        currentModsList = []
-        for i in os.listdir(os.getcwd()):
-            if os.path.isfile(i):
-                currentModsList.append(i)
-        currentModsDict = {
-            "mods":currentModsList,
-        }
-        json.dump(currentModsDict,f)
+if not os.path.isfile(os.getcwd() + "\\pythonTracker.json"):  #Checks to see if a history file already exists
+    with open(os.getcwd() + "\\pythonTracker.json") as JsonFile:  #Opens the modpack history file
+        if os.path.exists(os.getcwd() + "\\crash-reports"):  #Checks to see if there is a new crash report
+            with open(os.getcwd() + "\\crash-reports" + os.listdir(os.getcwd() + "\\crash-reports")[0]) as CrashFile:  #Opens the crash report file
+                changedMods = []  #Creates a new list to save any added/deleted mods since last run
+                oldJsonDict = json.load(JsonFile)  #Converts history file to a python dictionary
+                oldModsList = oldJsonDict["mods"]  #Saves the last known mods to a list
+                newModsList = os.listdir(os.getcwd() + "\\mods")  #Creates a list of all the current mods
+                if len(oldModsList) > len(newModsList): #Checks to see which way through the loop it should go
+                    for i in oldModsList:  #Loops through the old mod list as that one is longer
+                        if not i in newModsList:  #Checks to see if the current item in the old list is in the new list. If not, then we know it has been deleted
+                            changedMods.append(i)  #Adds the deleted mod to list of changed mods
+                else:
+                    for i in newModsList:  #Loops through the new mod list as that one is longer
+                        if not i in oldModsList:  #Checks to see if the current item in the new list is in the old list. If not, then we know it has been added
+                            changedMods.append(i)  #Adds the added mod to the list of changed mods
+
 
